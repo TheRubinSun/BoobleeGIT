e4025af64abc01ffb48f38592cf2bfd03bda2ea7
TheRubinSun <51780433+TheRubinSun@users.noreply.github.com>
2026-01-10 00:42:24 +0300

    0.1981v
    fix magic minion eqip slots size
    fix booster save

--- Assets/Scripts/Items/Boster.cs
+++ Assets/Scripts/Items/Boster.cs
@@ -1,11 +1,10 @@
 using System;
 using UnityEngine;
 
-[Serializable]
 public class Boster : Item, IUsable
 {
-    protected AllStats boosterType;
-    protected int countBoost;
+    public AllStats boosterType;
+    public int countBoost;
     private static int soundID = 3;
     public Boster(int id, string name, int maxCount, int spriteID, Quality quality, int cost, string description, AllStats bosterType, int countBoost, TypeItem typeItem = TypeItem.Other, bool isUse = false) : base(id, name, maxCount, spriteID, quality, cost, description, typeItem, isUse)
     {
